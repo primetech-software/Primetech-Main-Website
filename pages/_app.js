ee27654859cc9217adaed8825c59d8f45a215de5
Alex <hi.primetech@gmail.com>
2026-01-02 15:30:53 +0100

    feat(): Install microsoft clarity inside project

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout";
 import GlobalStyle from "../components/globalStyles";
 import LoadingScreen from "../components/LoadingScreen";
 import { Analytics } from "@vercel/analytics/next"
+import Clarity from '@microsoft/clarity';
+
 // import Router from 'next/router';
 // import NProgress from 'nprogress'; //nprogress module
 // import 'nprogress/nprogress.css'; //styles of nprogress
@@ -17,6 +19,12 @@ function MyApp({ Component, pageProps }) {
     // setLoading(true);
     setTimeout(() => setLoading(true), 1500);
   }, []);
+
+useEffect(() => {
+  if (typeof window !== "undefined") {
+    Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID);
+  }
+}, []);
 
 
   // Router.events.on('routeChangeStart', () => NProgress.start());
